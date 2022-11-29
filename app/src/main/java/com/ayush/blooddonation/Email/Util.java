3d5b0b1e49e66698f995fe49abf14a1bf71460c0
Ayush <ayush264raj@gmail.com>
2022-11-29 20:37:59 +0530

    Initial Commit

---
+++ app/src/main/java/com/ayush/blooddonation/Email/Util.java
@@ -0,0 +1,7 @@
+package com.ayush.blooddonation.Email;
+
+public class Util {
+
+    public  static final  String EMAIL="ar1232022@gmail.com";
+    public static final String PASSWORD="efylldpatcjsrlns";
+}
---
+++ app/src/main/java/com/ayush/blooddonation/Model/User.java
@@ -0,0 +1,92 @@
+package com.ayush.blooddonation.Model;
+
+public class User {
+    String Id,bloodgroup,email,idnumber,name,phonenumber,profilepictureurl,search,type;
+
+    public User() {
+    }
+
+    public User(String Id, String bloodgroup, String email, String idnumber, String name, String phonenumber, String profilepictureurl, String search, String type) {
+        this.Id=Id;
+        this.bloodgroup = bloodgroup;
+        this.email = email;
+        this.idnumber = idnumber;
+        this.name = name;
+        this.phonenumber = phonenumber;
+        this.profilepictureurl = profilepictureurl;
+        this.search = search;
+        this.type = type;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getBloodgroup() {
+        return bloodgroup;
+    }
+
+    public void setBloodgroup(String bloodgroup) {
+        this.bloodgroup = bloodgroup;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIdnumber() {
+        return idnumber;
+    }
+
+    public void setIdnumber(String idnumber) {
+        this.idnumber = idnumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getProfilepictureurl() {
+        return profilepictureurl;
+    }
+
+    public void setProfilepictureurl(String profilepictureurl) {
+        this.profilepictureurl = profilepictureurl;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
